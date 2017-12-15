83cbc80d8207ca929460114a58c2362afc7cacae
cestradaTesselar <cestrada@tesselar.mx>
2017-12-15 13:01:31 -0600

    feat evaluar

--- server.js
+++ server.js
@@ -27,11 +27,12 @@ app.get('/questions', (req, res) => {
 
 // Responde las preguntas
 app.post('/answers', (req, res) => {
-    res.send(req.body)
+    // res.send(req.body)
     fs.writeFileSync(`dump_${req.body.User.id}.txt`, JSON.stringify(req.body, null, '\t'), err => {
         if (err) throw err;
         console.log('File saved!');
     })
+    res.send(Evaluar(req.body.Respuestas))
 })
 
 // por default en el puerto 3008 para que el cliente corra en el 3000
@@ -74,4 +75,38 @@ const getPregunta = (i, size) => {
     if (i < size) return 0
     if (i >= size && i < size * 2) return 1
     if (i >= size * 2 && i < size * 3) return 2
+}
+const Evaluar = Respuestas => {
+    // console.log(Respuestas[0].Respuesta)
+
+    let eva = [0, 0, 0, 0, 0]
+
+    for (let i = 0; i < Respuestas.length - 1; i++) {
+        console.log(Respuestas[i], i)
+        if (Respuestas[i].Respuesta === 1) eva[0]++
+        if (Respuestas[i].Respuesta === 2) eva[1]++
+        if (Respuestas[i].Respuesta === 3) eva[2]++
+        if (Respuestas[i].Respuesta === 4) eva[3]++
+        if (Respuestas[i].Respuesta === 5) eva[4]++
+    }
+    let index = eva.indexOf(Math.max(...eva));
+
+    switch (index) {
+        case 1:
+            return "Para ti las palabras son poco significativas"
+            break;
+        case 2:
+            return "Para ti las palabras no tienen mucho poder"
+            break;
+        case 3:
+            return "Para ti las palabras tienen fuerza y valor, pero no son determinantes"
+            break;
+        case 4:
+            return "Las palabras son muy importantes para ti"
+            break;
+        case 5:
+            return "En ti las palabras ejercen mucho control y fuerza"
+            break;
+    }
+
 }
